dc5712a20701ca836d6175bf3d17d3f3a0e92d61
Shelby Guenthardt <sguenthardt11@gmail.com>
2016-12-15 20:45:13 +0000

    updated port for azure

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/', function(req,res){
    res.render('index');
 });
 
-app.listen(8080,function(){
+app.listen(process.env.PORT || 3000 || 8080,function(){
     console.log('App listening on port 8080');
 })
